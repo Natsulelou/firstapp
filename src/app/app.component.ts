53a7262fdfaf1880d4783663925f9696c5c12d71
Nutshapon <nut_ohlanla@hotmail.com>
2018-10-11 17:07:23 +0700

    [init]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  constructor(
+    private router: Router,
+  ){
+
+  }
+  openPage2(){
+    this.router.navigate[('/page2')];
+  }
+  goPage(){
+    this.router.navigate(['/page2']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page2Component } from './page/page2/page2.component';
+
+const routes: Routes = [
+  {
+    path: 'page2',
+    component: Page2Component
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    RouterModule.forRoot(
+      routes,
+      {enableTracing: false}),
   ],
   providers: [],
   bootstrap: [AppComponent]
